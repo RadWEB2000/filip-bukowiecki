5d7a25b90ba1a5310e136f2facfd20a9916aac27
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-01 14:42:09 +0200

    update document 02

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,32 @@
 import Document, {Html,Head,Main,NextScript,DocumentContext, DocumentInitialProps,} from "next/document";
 import { ServerStyleSheet } from "styled-components";
 export default class Page extends Document {
-    static async getInitialProps(ctx: DocumentContext):Promise<DocumentInitialProps | any> {
+
+    static async getInitialProps(ctx: DocumentContext):Promise<DocumentInitialProps> {
+
       const sheet = new ServerStyleSheet();
+
       const originalRenderPage = ctx.renderPage;
+
       try {
         ctx.renderPage = () =>
           originalRenderPage({
             enhanceApp: (App) => (props) =>
               sheet.collectStyles(<App {...props} />),
           });
+
         const initialProps = await Document.getInitialProps(ctx);
+
         return {
           ...initialProps,
-          styles: (
+          styles : (
             <>
               {initialProps.styles}
               {sheet.getStyleElement()}
             </>
           ),
         };
+
       } finally {
         sheet.seal();
       }
