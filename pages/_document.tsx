5b2152bb34dda7dde2961c7bdc048ea1af76c80f
Radosław Adamczyk <greatpolandweb@gmail.com>
2022-06-01 14:44:57 +0200

    add any

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,46 +1,33 @@
-import Document, {Html,Head,Main,NextScript,DocumentContext, DocumentInitialProps,} from "next/document";
-import { ServerStyleSheet } from "styled-components";
-export default class Page extends Document {
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document';
+import { ReactElement, ReactFragment } from 'react';
+import { ServerStyleSheet } from 'styled-components';
 
-    static async getInitialProps(ctx: DocumentContext):Promise<DocumentInitialProps> {
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<any> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
-      const sheet = new ServerStyleSheet();
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-      const originalRenderPage = ctx.renderPage;
-
-      try {
-        ctx.renderPage = () =>
-          originalRenderPage({
-            enhanceApp: (App) => (props) =>
-              sheet.collectStyles(<App {...props} />),
-          });
-
-        const initialProps = await Document.getInitialProps(ctx);
-
-        return {
-          ...initialProps,
-          styles : (
-            <>
-              {initialProps.styles}
-              {sheet.getStyleElement()}
-            </>
-          ),
-        };
-
-      } finally {
-        sheet.seal();
-      }
-    }
-    render() {
-      return (
-        <Html>
-          <Head>
-          </Head>
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-        </Html>
-      );
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
     }
   }
+}
